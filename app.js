1aad87e90d84ad85f1cb47d9c821621a85955f92
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-02-07 09:06:17 +0100

    node

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080.");
+});
