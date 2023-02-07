e85438951f90ab897c6056a01d47bf96b79b2e5f
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-02-07 11:21:27 +0100

    hhhh
    ghgggg

--- public/test.js
+++ public/test.js
@@ -1,23 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     let data = document.getElementById("data");
-    
-
-    function ourData(callback=function(){}){
-        const Http = new XMLHttpRequest();
-        const url='https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.943668/lat/59.43172/data.json';
-        Http.open("GET", url);
-        Http.send();
-        Http.onreadystatechange = function(){
-            if(Http.readyState === 4 && Http.status === 200) {
-                var response = JSON.parse(Http.responseText);
-                // console.log(response);
-                window.d = response;
-                callback()
-                return response;
-            };
-        };
-    };
 
 
     ourData(function(){
@@ -39,3 +22,25 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 })
+
+
+function search(searchWord) {
+    document.getElementById("map").src = 'https://maps.google.com/maps?q='+searchWord+'&t=k&z=9&ie=UTF8&iwloc=&output=embed'
+};
+
+
+function ourData(callback=function(){}){
+    const Http = new XMLHttpRequest();
+    const url='https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.943668/lat/59.43172/data.json';
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = function(){
+        if(Http.readyState === 4 && Http.status === 200) {
+            var response = JSON.parse(Http.responseText);
+            // console.log(response);
+            window.d = response;
+            callback()
+            return response;
+        };
+    };
+};
